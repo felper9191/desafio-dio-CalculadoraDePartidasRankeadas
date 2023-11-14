8e7b7787684ac7d0f72b3f00f4a3cca7af8342f7
felper9191 <84869849+felper9191@users.noreply.github.com>
2023-11-14 20:21:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+﻿let numeroVitorias = 122;
+let numeroDerrotas = 22;
+let saldoDeVitoriasJogador = 0;
+let nivelDoHeroi = "";
+
+function saldoDeVitorias (vitorias, derrotas) {
+    saldoDeVitoriasJogador = vitorias - derrotas;
+
+    return saldoDeVitoriasJogador;
+}
+
+function mostraMensagem (saldoDeVitorias, nivel) {
+    console.log("O Herói tem de saldo de " + saldoDeVitorias + ", está no nível de " + nivel + ".");
+}
+
+function confereNivel () {
+    if (saldoDeVitoriasJogador < 10) {       
+        nivelDoHeroi = "Ferro";
+    } else if (saldoDeVitoriasJogador >= 11 && saldoDeVitoriasJogador <= 20) {
+        nivelDoHeroi = "Bronze";
+    } else if (saldoDeVitoriasJogador >= 21 && saldoDeVitoriasJogador <= 50) {
+        nivelDoHeroi = "Prata";
+    } else if (saldoDeVitoriasJogador >= 51 && saldoDeVitoriasJogador <= 80) {
+        nivelDoHeroi = "Ouro";
+    } else if (saldoDeVitoriasJogador >= 81 && saldoDeVitoriasJogador <= 90) {
+        nivelDoHeroi = "Diamante";
+    } else if (saldoDeVitoriasJogador >= 91 && saldoDeVitoriasJogador <= 100) {
+        nivelDoHeroi = "Lendário";
+    } else {
+        nivelDoHeroi = "Imortal";
+    } 
+}
+
+
+saldoDeVitorias(numeroVitorias, numeroDerrotas);
+confereNivel();
+mostraMensagem(saldoDeVitoriasJogador, nivelDoHeroi);
